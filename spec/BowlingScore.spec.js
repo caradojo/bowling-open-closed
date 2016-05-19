3219d1c465b7471f1362f9f40abe29d07cb00400
Johan Martinsson <martinsson.johan@gmail.com>
2016-05-20 01:27:56 +0200

    did strike

--- spec/BowlingScore.spec.js
+++ spec/BowlingScore.spec.js
@@ -14,11 +14,14 @@ describe('bowling', function () {
         expect(scoreFor("42 81 00 00 00 00 00 00 00 00")).to.equal(6 + 9)
     })
 
-    it.skip('strike', function () {
+    it('strike', function () {
         expect(scoreFor("60 30 X 45 00 00 00 00 00 00")).to.equal(6+ 3 + 19 + 9)
     })
 
     function parseToInteger(n) {
+        if (n === 'X') {
+            return 10
+        }
         return Number.parseInt(n)
     }
 
--- src/BowlingScore.js
+++ src/BowlingScore.js
@@ -1,22 +1,38 @@
 var _ = require('lodash')
 
 function BowlingScore() {
-}
 
+}
 
 function sum(a, b) {
     return a + b
 }
 
+function isStrike(remainingRolls) {
+    return remainingRolls[0] == 10
+}
+
+function isSpare(remainingRolls) {
+    return remainingRolls[0] + remainingRolls[1] == 10
+}
+
 function recursiveFrameScore(remainingRolls) {
     if (remainingRolls.length === 0) return []
+
     var currentFrameScore;
-    if (remainingRolls[0] + remainingRolls[1] == 10) {
+    var nextRemainingRolls;
+    if (isStrike(remainingRolls)) {
+        currentFrameScore = 10 + remainingRolls[1] + remainingRolls[2]
+        nextRemainingRolls = _.drop(remainingRolls, 1);
+    }
+    else if (isSpare(remainingRolls)) {
         currentFrameScore = 10 + remainingRolls[2]
+        nextRemainingRolls = _.drop(remainingRolls, 2);
     } else {
         currentFrameScore = remainingRolls[0] + remainingRolls[1];
+        nextRemainingRolls = _.drop(remainingRolls, 2);
     }
-    var nextRemainingRolls = _.drop(remainingRolls, 2)
+
     return [currentFrameScore].concat(recursiveFrameScore(nextRemainingRolls))
 }
 
