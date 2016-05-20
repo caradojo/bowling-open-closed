7ae574c06af5e2a1a50e793b176c4845694cfc60
Johan Martinsson <martinsson.johan@gmail.com>
2016-05-20 23:38:20 +0200

    extracted object strike

--- src/BowlingScore.js
+++ src/BowlingScore.js
@@ -8,22 +8,39 @@ function sum(a, b) {
     return a + b
 }
 
+
 function isStrike(remainingRolls) {
     return remainingRolls[0] == 10
+}
+function scoreAndRemainingRolls(currentFrameScore, nextRemainingRolls) {
+    return {currentFrameScore: currentFrameScore, nextRemainingRolls: nextRemainingRolls}
+}
+function applyStrike(remainingRolls) {
+    var currentFrameScore = 10 + remainingRolls[1] + remainingRolls[2]
+    var nextRemainingRolls = _.drop(remainingRolls, 1);
+    return scoreAndRemainingRolls(currentFrameScore, nextRemainingRolls)
+}
+var strikeCalculator = {
+    matches: isStrike,
+    calculateScore: applyStrike
 }
 
 function isSpare(remainingRolls) {
     return remainingRolls[0] + remainingRolls[1] == 10
 }
-
 function recursiveFrameScore(remainingRolls) {
     if (remainingRolls.length === 0) return []
 
+
     var currentFrameScore;
     var nextRemainingRolls;
-    if (isStrike(remainingRolls)) {
-        currentFrameScore = 10 + remainingRolls[1] + remainingRolls[2]
-        nextRemainingRolls = _.drop(remainingRolls, 1);
+    var __ret;
+
+
+    if (strikeCalculator.matches(remainingRolls)) {
+        __ret = strikeCalculator.calculateScore(remainingRolls);
+        currentFrameScore = __ret.currentFrameScore
+        nextRemainingRolls = __ret.nextRemainingRolls
     }
     else if (isSpare(remainingRolls)) {
         currentFrameScore = 10 + remainingRolls[2]
