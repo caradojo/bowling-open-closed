e4e85d4e31f77988b49aeed67ca764445f762e0b
Remy Sanlaville <remy.sanlaville@gmail.com>
2016-05-21 23:04:02 +0200

    Add score_is_the_sum_of_pins_knocked_down unit test
    upgrade zohhak version to 1.1.1
    add toList zohhak coercion in order to be able to define an array of pins in the parameters test

--- src/test/java/org/cara/dojo/openclose/bowling/BowlingGameTest.java
+++ src/test/java/org/cara/dojo/openclose/bowling/BowlingGameTest.java
@@ -2,7 +2,20 @@ package org.cara.dojo.openclose.bowling;
 
 import org.fest.assertions.api.Assertions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import com.googlecode.zohhak.api.Coercion;
+import com.googlecode.zohhak.api.TestWith;
+
+import ch.lambdaj.collection.LambdaList;
+import ch.lambdaj.function.convert.Converter;
+import static ch.lambdaj.collection.LambdaCollections.with;
+
+import java.util.List;
+
+import com.googlecode.zohhak.api.runners.ZohhakRunner;
+
+@RunWith(ZohhakRunner.class)
 public class BowlingGameTest {
 
   @Test
@@ -16,5 +29,33 @@ public class BowlingGameTest {
     
     // Then
     Assertions.assertThat(score).isEqualTo(0);
+  }
+
+  @TestWith(value="[1, 1, 1, 1] ; 4", separator=";")
+  public void score_is_the_sum_of_pins_knocked_down(List<Integer> pinsGame, Integer expectedScore) {
+    //Given
+    BowlingGame bowling = new BowlingGame();
+    for (int pins : pinsGame) {
+      bowling.roll(pins);
+    }
+    
+    // When
+    int score = bowling.score();
+    
+    // Then
+    Assertions.assertThat(score).isEqualTo(expectedScore);
+  }
+  
+  
+  @Coercion
+  public LambdaList<Integer> toList(String input) {
+    String[] numbers = input.replace("[", "").replace("]", "").split(",");
+    
+    return with(numbers).convert(new Converter<String, Integer>() {
+      
+      public Integer convert(String from) {
+        return Integer.parseInt(from.trim());
+      }
+    });
   }
 }
