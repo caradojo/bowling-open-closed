9ed59b3c6c809c7d4de46f021a0556ca7b97af52
Remy Sanlaville <remy.sanlaville@gmail.com>
2016-05-21 11:00:59 +0200

    Init branch

---
+++ src/test/java/org/cara/dojo/openclose/bowling/BowlingTest.java
@@ -0,0 +1,12 @@
+package org.cara.dojo.openclose.bowling;
+
+import static org.fest.assertions.api.Assertions.fail;
+import org.junit.Test;
+
+public class BowlingTest {
+
+  @Test
+  public void checkEnvironment() {
+    fail("check environment");
+  }
+}
