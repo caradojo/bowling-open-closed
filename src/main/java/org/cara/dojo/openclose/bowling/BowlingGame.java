5fad571c71a3a4552f77fb664d2b99cf091cad14
Remy Sanlaville <remy.sanlaville@gmail.com>
2016-05-21 22:44:55 +0200

    Simple implementation for score is 0 when 0 pins

--- src/main/java/org/cara/dojo/openclose/bowling/BowlingGame.java
+++ src/main/java/org/cara/dojo/openclose/bowling/BowlingGame.java
@@ -3,12 +3,11 @@ package org.cara.dojo.openclose.bowling;
 public class BowlingGame {
 
   public void roll(int pins) {
-    throw new UnsupportedOperationException();
-    
+
   }
 
   public int score() {
-    throw new UnsupportedOperationException();
+    return 0;
   }
 
 }
