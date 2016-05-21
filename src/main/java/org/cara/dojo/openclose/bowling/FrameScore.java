01df199b9356adcd533bd391a6d477b641ed7764
Remy Sanlaville <remy.sanlaville@gmail.com>
2016-05-22 01:12:43 +0200

    Introduce FrameScore concept with frame_score_is_the_sum_of_two_pins unit test

---
+++ src/main/java/org/cara/dojo/openclose/bowling/FrameScore.java
@@ -0,0 +1,14 @@
+package org.cara.dojo.openclose.bowling;
+
+public class FrameScore {
+  
+  public void roll(Integer pins) {
+    throw new UnsupportedOperationException();
+    
+  }
+
+  public Integer score() {
+    throw new UnsupportedOperationException();
+  }
+
+}
---
+++ src/test/java/org/cara/dojo/openclose/bowling/FrameScoreTest.java
@@ -0,0 +1,48 @@
+package org.cara.dojo.openclose.bowling;
+
+import static ch.lambdaj.collection.LambdaCollections.with;
+
+import java.util.List;
+
+import org.fest.assertions.api.Assertions;
+import org.junit.runner.RunWith;
+
+import com.googlecode.zohhak.api.Coercion;
+import com.googlecode.zohhak.api.TestWith;
+import com.googlecode.zohhak.api.runners.ZohhakRunner;
+
+import ch.lambdaj.collection.LambdaList;
+import ch.lambdaj.function.convert.Converter;
+
+@RunWith(ZohhakRunner.class)
+public class FrameScoreTest {
+
+  @TestWith(value={"[0, 0] ; 0", "[1, 5] ; 6", "[5, 4] ; 9"}, separator=";")
+  public void frame_score_is_the_sum_of_two_pins(List<Integer> pinsGame, Integer expectedScore) {
+    //Given
+    FrameScore frame = new FrameScore();
+    
+    for (int pins : pinsGame) {
+      frame.roll(pins);
+    }
+    
+    // When
+    int score = frame.score();
+    
+    // Then
+    Assertions.assertThat(score).isEqualTo(expectedScore);
+  }
+  
+  
+  @Coercion
+  public LambdaList<Integer> toList(String input) {
+    String[] numbers = input.replace("[", "").replace("]", "").split(",");
+    
+    return with(numbers).convert(new Converter<String, Integer>() {
+      
+      public Integer convert(String from) {
+        return Integer.parseInt(from.trim());
+      }
+    });
+  }
+}
