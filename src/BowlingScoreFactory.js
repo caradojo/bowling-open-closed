06b422845f9c58ba21ee75d2f20a2608081bee9e
Johan Martinsson <martinsson.johan@gmail.com>
2016-06-25 15:24:55 +0200

    passed martian spare

--- src/BowlingScoreFactory.js
+++ src/BowlingScoreFactory.js
@@ -9,7 +9,7 @@ function normalBowling() {
 
 
 function martianBowling() {
-    var allFrameTypes = [martianStrikeInLastFrame, new strikeFrame(), martianSpareInLastFrame, new martianSpareFrame(), new martianNormalFrame()]
+    var allFrameTypes = [martianStrikeInLastFrame, new strikeFrame(), martianSpareInLastFrame, martianSpareFrame, new martianNormalFrame()]
     return new BowlingScore(allFrameTypes)
 }
 
--- src/FrameTypes.js
+++ src/FrameTypes.js
@@ -1,5 +1,27 @@
 import * as _ from 'lodash'
 
+
+function appliesWhen(predicate1, predicate2, etc) {
+    return {
+        sumsUpNextRolls: function (numberOfRolls) {
+            return {
+                removesRolls: function (numberOfRollsToRemove) {
+                    return {
+                        matches: function (rolls) {
+                            return predicate1(rolls) && predicate2(rolls)
+                        },
+                        calculateScore: function (rolls) {
+                            var frameScore = sumOfNext(numberOfRolls, rolls)
+                            var nextRolls = _.drop(rolls, numberOfRollsToRemove);
+                            return scoreAndRemainingRolls(frameScore, nextRolls)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 class strikeFrame {
     matches(rolls) {
         return isStrike(rolls)
@@ -72,41 +94,11 @@ class martianNormalFrame {
     }
 }
 
-class martianSpareFrame {
-    matches(rolls) {
-        return sumOfNext(3, rolls) == 10
-    }
 
-    calculateScore(rolls) {
-        var frameScore = sumOfNext(4, rolls)
-        var nextRolls = _.drop(rolls, 3);
-        return scoreAndRemainingRolls(frameScore, nextRolls)
-    }
-}
-function isMartianSpare(rolls) {
-    return sumOfNext(3, rolls) >= 10
-}
-
-function appliesWhen(predicate1, predicate2, etc) {
-    return {
-        sumsUpNextRolls: function (numberOfRolls) {
-            return {
-                removesRolls: function (numberOfRollsToRemove) {
-                    return {
-                        matches: function (rolls) {
-                            return predicate1(rolls) && predicate2(rolls)
-                        },
-                        calculateScore: function (rolls) {
-                            var frameScore = sumOfNext(numberOfRolls, rolls)
-                            var nextRolls = _.drop(rolls, numberOfRollsToRemove);
-                            return scoreAndRemainingRolls(frameScore, nextRolls)
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
+var martianSpareFrame =
+    appliesWhen(isMartianSpare, () => true)
+        .sumsUpNextRolls(4)
+        .removesRolls(3)
 
 var martianStrikeInLastFrame =
     appliesWhen(isStrike, isLastMartianFrame)
@@ -117,6 +109,11 @@ var martianSpareInLastFrame =
     appliesWhen(isMartianSpare, isLastMartianFrame)
         .sumsUpNextRolls(4)
         .removesRolls(4)
+
+
+function isMartianSpare(rolls) {
+    return sumOfNext(3, rolls) >= 10
+}
 
 
 function isSpare(rolls) {
